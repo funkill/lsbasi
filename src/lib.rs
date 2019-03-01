312564e4e9fb05d988584c81a02e0f4c0c825c3b
funkill2 <funkill2@gmail.com>
2019-03-02 00:00:35 +0300

    evaluating sequences

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![feature(try_trait)]
+
+use std::{
+    fmt::{Display, Formatter},
+    num::ParseIntError,
+    option::NoneError,
+};
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 enum Type {
     Whitespace,
@@ -9,38 +17,77 @@ enum Type {
     Eof,
 }
 
-#[derive(Debug, Eq, PartialEq)]
 struct Token {
     _type: Type,
     value: Option<String>,
 }
 
-pub struct Interpreter {}
-
-impl Interpreter {
-    pub fn evaluate(text: &str) -> i64 {
-        let stream = Self::tokenize(text);
-        assert_eq!(stream.len(), 4, "Wrong parsing input!");
-        assert_eq!(stream[3]._type, Type::Eof, "Wrong end of line!");
-        assert_eq!(stream[0]._type, Type::Integer, "Wrong first operand type!");
-        assert_eq!(stream[2]._type, Type::Integer, "Wrong second operand type!");
-
-        let first = stream.get(0).unwrap().value.clone().unwrap().parse::<i64>().unwrap();
-        let second = stream.get(2).unwrap().value.clone().unwrap().parse::<i64>().unwrap();
-
-        match stream.get(1).unwrap()._type.clone() {
-            Type::Plus => first + second,
-            Type::Minus => first - second,
-            Type::Mul => first * second,
-            Type::Div => {
-                assert_ne!(second, 0, "Division by zero!");
-                first / second
-            },
-            _type @ _ => panic!("Unknown operation `{:?}`", _type),
+impl Token {
+    fn is_ops(&self) -> bool {
+        match self._type {
+            Type::Minus | Type::Plus | Type::Mul | Type::Div => true,
+            _ => false,
         }
     }
 
-    fn tokenize(text: &str) -> Vec<Token> {
+    fn is_eof(&self) -> bool {
+        self._type == Type::Eof
+    }
+}
+
+pub struct Interpreter {}
+
+impl Interpreter {
+    pub fn evaluate(text: impl ToString) -> Result<Option<i64>, Error> {
+        let stream = Self::tokenize(text.to_string());
+        if let Some(first) = stream.first() {
+            if first.is_eof() {
+                return Ok(None);
+            }
+
+            if first.is_ops() {
+                return Err(Error::with_message("Evaluated text must starts with integer!"));
+            }
+
+            let mut first = parse_number(first)?;
+            let mut stream = stream.iter().skip(1);
+            while let Some(op) = stream.next() {
+                if op._type == Type::Eof {
+                    break;
+                }
+
+                if !op.is_ops() {
+                    return Err(Error::with_message("Item must be operand!"));
+                }
+
+                let second = match stream.next() {
+                    Some(token) => token,
+                    None => return Err(Error::with_message("Unexpected end of stream")),
+                };
+                let second = parse_number(&second)?;
+
+                match &op._type {
+                    Type::Plus => first += second,
+                    Type::Minus => first -= second,
+                    Type::Mul => first *= second,
+                    Type::Div => {
+                        if second == 0 {
+                            return Err(Error::with_message("Division by zero!"));
+                        }
+
+                        first /= second
+                    }
+                    _ => unreachable!(),
+                }
+            }
+
+            Ok(Some(first))
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn tokenize(text: String) -> Vec<Token> {
         let mut tokens = vec![];
         let mut chars = text.chars().into_iter().peekable();
 
@@ -53,25 +100,193 @@ impl Interpreter {
                 }
 
                 val.push(curr);
-                match chars
+                if chars
                     .peek()
                     .map(detect_char_type)
                     .filter(|next_type| next_type == &curr_type)
+                    .is_none()
                 {
-                    Some(_) => {}
-                    None => {
-                        let token = Token {
-                            _type: curr_type,
-                            value: Some(val),
-                        };
-                        tokens.push(token);
-                        break;
-                    }
+                    let token = Token {
+                        _type: curr_type,
+                        value: Some(val),
+                    };
+                    tokens.push(token);
+
+                    break;
                 }
             }
         }
 
         tokens
+    }
+}
+
+#[cfg(test)]
+mod evaluate_tests {
+    use super::Interpreter;
+
+    macro_rules! add_test {
+        ($($name:ident: $eval:expr, result: $res:expr,)+) => {
+            $(
+                #[test]
+                fn $name() {
+                    let result = Interpreter::evaluate($eval).unwrap().unwrap();
+                    assert_eq!(result, $res);
+                }
+            )+
+        };
+    }
+
+    add_test!(
+        eval: "1+2\n", result: 3,
+        eval_0: " 1+2\n", result: 3,
+        eval_1: "1 +2\n", result: 3,
+        eval_2: "1+ 2\n", result: 3,
+        eval_3: "1+2 \n", result: 3,
+        eval_4: " 1 +2\n", result: 3,
+        eval_5: " 1+ 2\n", result: 3,
+        eval_6: " 1+2 \n", result: 3,
+        eval_7: "12+2\n", result: 14,
+        eval_8: "1-2\n", result: -1,
+        eval_9: "1*2\n", result: 2,
+        eval_10: "4/2\n", result: 2,
+        eval_11: "4 /2 * 5 + 5 - 3", result: 12,
+    );
+
+    #[test]
+    fn check_division_by_zero() {
+        let result = Interpreter::evaluate("1/0");
+
+        assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn evaluate_empty_string() {
+        let result = Interpreter::evaluate("");
+
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap().is_none(), true);
+
+        let result = Interpreter::evaluate("\n");
+
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap().is_none(), true);
+    }
+
+    macro_rules! check_strating_with_ops {
+        ($({ $name:ident, eval: $eval:expr },)+) => {
+            $(
+                #[test]
+                fn $name() {
+                    let result = Interpreter::evaluate($eval);
+                    assert_eq!(result.is_err(), true);
+                }
+            )+
+        };
+    }
+
+    check_strating_with_ops!(
+        {starting_with_add, eval: "+12"},
+        {starting_with_sub, eval: "-12"},
+        {starting_with_mul, eval: "*12"},
+        {starting_with_div, eval: "/12"},
+    );
+
+    #[test]
+    fn check_non_ops_operator() {
+        let result = Interpreter::evaluate("1 1 +");
+
+        assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn check_forgotten_second_operand() {
+        let result = Interpreter::evaluate("1+");
+
+        assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn check_wrong_first_argument() {
+        let result = Interpreter::evaluate(" +1");
+
+        assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn check_wrong_second_argument() {
+        let result = Interpreter::evaluate("1++");
+
+        assert_eq!(result.is_err(), true);
+    }
+}
+
+fn parse_number(token: &Token) -> Result<i64, Error> {
+    if token.is_ops() || token._type == Type::Eof || token._type == Type::Whitespace {
+        return Err(Error::with_message("Evaluated text must be integer!"));
+    }
+
+    token.value.clone()?.parse().map_err(Into::into)
+}
+
+#[cfg(test)]
+mod parse_number_tests {
+    use super::{parse_number, Token, Type};
+
+    #[test]
+    fn parsing_number() {
+        let token = Token {
+            _type: Type::Integer,
+            value: Some("0123456789".into()),
+        };
+        let value = parse_number(&token);
+
+        assert_eq!(value.is_ok(), true);
+        assert_eq!(value.ok(), Some(123456789));
+    }
+
+    macro_rules! parsing_non_integers {
+        ($({ name: $name:ident, type: $type:expr},)+) => {
+            $(
+                #[test]
+                fn $name() {
+                    let token = Token { _type: $type, value: None };
+                    let err = parse_number(&token);
+                    assert_eq!(err.is_err(), true);
+                }
+            )+
+        };
+    }
+
+    parsing_non_integers!(
+        { name: parse_add, type: Type::Plus },
+        { name: parse_sub, type: Type::Minus },
+        { name: parse_mul, type: Type::Mul },
+        { name: parse_div, type: Type::Div },
+        { name: parse_eof, type: Type::Eof },
+        { name: parse_whitespace, type: Type::Whitespace },
+    );
+
+    #[test]
+    fn parse_none_value() {
+        let token = Token {
+            _type: Type::Integer,
+            value: None,
+        };
+        let value = parse_number(&token);
+
+        assert_eq!(value.is_err(), true);
+    }
+
+    #[test]
+    fn parse_wrong_value() {
+        let token = Token {
+            _type: Type::Integer,
+            value: Some("abc".into()),
+        };
+        let value = parse_number(&token);
+
+        assert_eq!(value.is_err(), true);
     }
 }
 
@@ -190,41 +405,48 @@ mod detect_char_type_tests {
     }
 }
 
-#[cfg(test)]
-mod evaluate_tests {
-    use super::Interpreter;
+#[derive(Debug)]
+pub struct Error {
+    repr: Repr,
+}
 
-    macro_rules! add_test {
-        ($($name:ident: $eval:expr, result: $res:expr,)+) => {
-            $(
-                #[test]
-                fn $name() {
-                    let result = Interpreter::evaluate($eval);
-                    assert_eq!(result, $res);
-                }
-            )+
-        };
+impl Error {
+    fn with_message(message: impl ToString) -> Self {
+        let repr = Repr::Inner(message.to_string());
+
+        Error { repr }
     }
+}
 
-    add_test!(
-        eval: "1+2\n", result: 3,
-        eval_0: " 1+2\n", result: 3,
-        eval_1: "1 +2\n", result: 3,
-        eval_2: "1+ 2\n", result: 3,
-        eval_3: "1+2 \n", result: 3,
-        eval_4: " 1 +2\n", result: 3,
-        eval_5: " 1+ 2\n", result: 3,
-        eval_6: " 1+2 \n", result: 3,
-        eval_7: "12+2\n", result: 14,
-        eval_8: "1-2\n", result: -1,
-        eval_9: "1*2\n", result: 2,
-        eval_10: "4/2\n", result: 2,
-    );
+#[derive(Debug)]
+enum Repr {
+    Inner(String),
+    NoneError(NoneError),
+    Other(Box<std::error::Error>),
+}
 
-    #[test]
-    #[should_panic]
-    fn check_division_by_zero() {
-        Interpreter::evaluate("1/0");
+impl std::error::Error for Error {}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+        match &self.repr {
+            Repr::Inner(message) => write!(f, "{}", message),
+            Repr::NoneError(_) => write!(f, "Trying to unwrap None"),
+            Repr::Other(e) => e.fmt(f),
+        }
     }
+}
 
+impl From<NoneError> for Error {
+    fn from(e: NoneError) -> Self {
+        let repr = Repr::NoneError(e);
+        Error { repr }
+    }
+}
+
+impl From<ParseIntError> for Error {
+    fn from(e: ParseIntError) -> Self {
+        let repr = Repr::Other(Box::new(e));
+        Error { repr }
+    }
 }
